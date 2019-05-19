0661055ac367f74b499c8da08ed2e72bc7aec91b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2019-05-19 18:36:55 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const nav = document.querySelector("nav")
 const menu_btn_line = document.querySelectorAll(".menu-btn-line")
 const menuOpenClass = document.querySelectorAll(".menuOpen")
 menu.addEventListener("click",menuOpen)
+menu.addEventListener("touchstart",menuOpen)
 function menuOpen () {
   menu.classList.toggle("active")
   nav.classList.toggle("active-nav")
